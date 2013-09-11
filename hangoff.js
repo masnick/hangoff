190652c990c8fb49ceb0ba7e8d262b4b267fec2d
Max Masnick <max@masnick.me>
2013-09-11 17:21:27 -0400

    initial commit

---
+++ hangoff.js
@@ -0,0 +1,16 @@
+// http://stackoverflow.com/questions/3387427/javascript-remove-element-by-id
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = 0, len = this.length; i < len; i++) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
+setTimeout(function() {
+  document.getElementById("content").remove();
+  document.getElementById("gb").remove();
+}, 4000);
